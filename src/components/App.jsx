824b954037fe76d54d7f67f2c5ce40adb63fb746
destawala <58984482+destawala@users.noreply.github.com>
2021-10-05 09:32:05 +0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+function App() {
+  let [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  function getTime() {
+    setTime(new Date().toLocaleTimeString());
+  }
+  return (
+    <div className="container">
+      <h1>{time}</h1>
+      <button onClick={getTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
